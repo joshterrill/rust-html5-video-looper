682f791293d370e027795e65d6eb1a6edbcf7a66
Josh Terrill <joshterrill.dev@gmail.com>
2019-11-25 19:24:14 -0800

    beginning of error handling changes

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,49 @@ use ws::{listen, Message, Handler, Sender};
 use serde::{Deserialize, Serialize};
 use serde_json;
 
+struct Error {
+    message: String
+}
+
 struct Server {
     ws: Sender,
     file_path: String,
-    max_plays: u8
+    max_plays: usize
 }
 
 #[derive(Serialize, Deserialize)]
 struct ClientPayload {
-    max_plays: u8,
-    file_path: String
+    max_plays: usize,
+    file_path: String,
+    error: Option<String>
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+impl<T: std::error::Error> From<T> for Error {
+    fn from(err: T) -> Self {
+        Error { message: err.description().to_owned() }
+    }
 }
 
 impl Handler for Server {
-    fn on_message(&mut self, _msg: Message) -> ws::Result<()> {
-        let item = ClientPayload {
-            max_plays: self.max_plays,
-            file_path: get_file(self.file_path.to_owned())
-        };
-        self.ws.send(serde_json::to_string(&item).unwrap()).unwrap();
-        Ok(())
+    fn on_message(&mut self, _msg: Message) -> Result<String> {
+       match get_file(self.file_path.to_owned()) {
+           Some(file) => {
+            let item = ClientPayload {
+                max_plays: self.max_plays,
+                file_path: file,
+                error: None
+            };
+            self.ws.send(serde_json::to_string(&item).unwrap()).unwrap();
+            Ok("Ok".to_owned())
+           },
+           Err(error) => {
+            println!("Error: {:?}", error.message);
+            Ok("Error".to_owned())
+           }
+       }
+        
     }
 }
 
@@ -34,11 +57,11 @@ fn generate_random_number(count: usize) -> usize {
     rand::thread_rng().gen_range(0, count)
 }
 
-fn get_file(file_path: String) -> String {
+fn get_file(file_path: String) -> Result<String> {
     let paths = fs::read_dir(file_path).unwrap();
     let vec: Vec<_> = paths.map(|path| path.unwrap().path()).collect();
     let random_path = vec.get(generate_random_number(vec.len()));
-    random_path.unwrap().display().to_string()
+    Ok(random_path.unwrap().display().to_string())
 }
 
 fn init_rocket() {
@@ -47,7 +70,7 @@ fn init_rocket() {
     .launch();
 }
 
-fn init_websockets(file_path: String, max_plays: u8) {
+fn init_websockets(file_path: String, max_plays: usize) {
     listen("127.0.0.1:3012", |out| {
         Server {
             ws: out,
@@ -70,7 +93,7 @@ fn main () {
     }));
 
     threads.push(thread::spawn(move || {
-        init_websockets(file_path, max_plays.parse::<u8>().unwrap());
+        init_websockets(file_path, max_plays.parse::<usize>().unwrap());
     }));
 
     for thread in threads {
