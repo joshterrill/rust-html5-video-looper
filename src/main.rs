7bf6e0acfc41492574090fe21bf35b008e32477d
Josh Terrill <joshterrill.dev@gmail.com>
2020-05-02 23:19:35 -0700

    fixed issue with ws result return

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,57 @@
+extern crate rocket_contrib;
 extern crate ws;
 
-use rocket_contrib::serve::StaticFiles;
 use rand::Rng;
-use std::{fs, thread, env};
-use ws::{listen, Message, Handler, Sender};
+use rocket_contrib::serve::StaticFiles;
 use serde::{Deserialize, Serialize};
 use serde_json;
+use std::{env, fs, thread};
+use ws::{listen, Handler, Message, Sender};
 
 struct Error {
-    message: String
+    message: String,
 }
 
 struct Server {
     ws: Sender,
     file_path: String,
-    max_plays: usize
+    max_plays: usize,
 }
 
 #[derive(Serialize, Deserialize)]
 struct ClientPayload {
     max_plays: usize,
     file_path: String,
-    error: Option<String>
+    error: Option<String>,
 }
 
-type Result<T> = std::result::Result<T, Error>;
+// type Result<T> = std::result::Result<T, Error>;
 
 impl<T: std::error::Error> From<T> for Error {
     fn from(err: T) -> Self {
-        Error { message: err.description().to_owned() }
+        Error {
+            message: err.description().to_owned(),
+        }
     }
 }
 
 impl Handler for Server {
-    fn on_message(&mut self, _msg: Message) -> Result<String> {
-       match get_file(self.file_path.to_owned()) {
-           Some(file) => {
-            let item = ClientPayload {
-                max_plays: self.max_plays,
-                file_path: file,
-                error: None
-            };
-            self.ws.send(serde_json::to_string(&item).unwrap()).unwrap();
-            Ok("Ok".to_owned())
-           },
-           Err(error) => {
-            println!("Error: {:?}", error.message);
-            Ok("Error".to_owned())
-           }
-       }
-        
+    fn on_message(&mut self, _msg: Message) -> ws::Result<()> {
+        match get_file(self.file_path.to_owned()) {
+            Ok(file) => {
+                let item = ClientPayload {
+                    max_plays: self.max_plays,
+                    file_path: file,
+                    error: None,
+                };
+                self.ws.send(serde_json::to_string(&item).unwrap()).unwrap();
+                Ok(())
+            }
+            Err(error) => {
+                // println!("Error: {:?}", error.message);
+                Ok(())
+            }
+        }
     }
 }
 
@@ -66,22 +68,20 @@ fn get_file(file_path: String) -> Result<String> {
 
 fn init_rocket() {
     rocket::ignite()
-    .mount("/", StaticFiles::from("public"))
-    .launch();
+        .mount("/", StaticFiles::from("public"))
+        .launch();
 }
 
 fn init_websockets(file_path: String, max_plays: usize) {
-    listen("127.0.0.1:3012", |out| {
-        Server {
-            ws: out,
-            file_path: file_path.to_owned(),
-            max_plays,
-        }
-    }).unwrap();
+    listen("127.0.0.1:3012", |out| Server {
+        ws: out,
+        file_path: file_path.to_owned(),
+        max_plays,
+    })
+    .unwrap();
 }
 
-fn main () {
-    
+fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = args[2].to_string();
     let max_plays = args[4].to_string();
@@ -99,5 +99,4 @@ fn main () {
     for thread in threads {
         let _ = thread.join();
     }
-
 }
